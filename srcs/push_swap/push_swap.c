d899e1a5eab646cd167d5791662882960b79b7cf
Victor <victor.dedios@outlook.com>
2021-04-17 21:28:54 +0200

    from scratch

--- srcs/push_swap/push_swap.c
+++ srcs/push_swap/push_swap.c
@@ -29,8 +29,6 @@ static t_element *mid_el(t_element *start, int len)
     it = 0;
     stack = extract_stack(start, len);
     stack = merge_sort(stack);
-    //lst_print("[STACK] ", stack);
-    //printf("is_sorted: %d\n", is_sorted(stack, NULL, UP));
     while (it < len / 2)
     {
         stack = stack->next;
@@ -39,163 +37,9 @@ static t_element *mid_el(t_element *start, int len)
     return (stack);
 }
 
-static int split_a(t_element **stack_a, t_element **stack_b,
-                    int len, t_element *pivot)
-{
-    int rot_rev;
-    int split_len;
-    int half_len;
-    int stack_len;
-    int init_len;
-
-    stack_len = lst_len(*stack_a);
-    init_len = len;
-    half_len = len / 2;
-    rot_rev = 0;
-    split_len = 0;
-    if (*stack_a)
-    {
-        while (len)
-        {
-            if (split_len == half_len)
-                break;
-            if ((*stack_a)->value < pivot->value)
-            {
-                push_b(stack_a, stack_b);
-                ft_putstr_fd("pb\n", 1);
-                split_len++;
-            }
-            else
-            {
-                if (stack_len > init_len)
-                    rot_rev++;
-                rot_a(stack_a, stack_b);
-                ft_putstr_fd("ra\n", 1);
-            }
-            len--;
-        }
-    }
-    while (rot_rev)
-    {
-        rot_rev_a(stack_a, stack_b);
-        ft_putstr_fd("rra\n", 1);
-        rot_rev--;
-    }
-    return (split_len);
-}
-
-static int split_b(t_element **stack_a, t_element **stack_b,
-                    int len, t_element *pivot)
-{
-    int rot_rev;
-    int split_len;
-    int half_len;
-    int stack_len;
-    int init_len;
-
-    stack_len = lst_len(*stack_b);
-    init_len = len;
-    half_len = len / 2;
-    rot_rev = 0;
-    rot_rev = 0;
-    split_len = 0;
-    if (*stack_b)
-    {
-        while (len)
-        {
-            if (split_len == half_len)
-                break;
-            if ((*stack_b)->value > pivot->value)
-            {
-                push_a(stack_a, stack_b);
-                ft_putstr_fd("pa\n", 1);
-                split_len++;
-            }
-            else
-            {
-               if (stack_len > init_len)
-                    rot_rev++;
-                rot_b(stack_a, stack_b);
-                ft_putstr_fd("rb\n", 1);
-            }
-            len--;
-        }
-    }
-    while (rot_rev)
-    {
-        rot_rev_b(stack_a, stack_b);
-        ft_putstr_fd("rrb\n", 1);
-        rot_rev--;
-    }
-    return (split_len);
-}
-
-static void undo_split_a(t_element **stack_a, t_element **stack_b,
-                         int len)
-{
-    while (len)
-    {
-        push_b(stack_a, stack_b);
-        ft_putstr_fd("pb\n", 1);
-        len--;
-    }
-}
-
-static void undo_split_b(t_element **stack_a, t_element **stack_b,
-                         int len)
-{
-    while (len)
-    {
-        push_a(stack_a, stack_b);
-        ft_putstr_fd("pa\n", 1);
-        len--;
-    }
-}
-
 static void push_swap(t_element **stack_a, t_element **stack_b,
                         int len, char stack_id)
 {
-    t_element   *pivot;
-    int         split_len;
-
-    if (stack_id == 'a' && len == 2)
-    {
-        if ((*stack_a)->value > ((t_element *)(*stack_a)->next)->value)
-        {
-            swap_a(stack_a, stack_b);
-            ft_putstr_fd("sa\n", 1);
-        }
-    }
-    else if (stack_id == 'a' && len > 2)
-    {
-        if (!is_sorted(*stack_a, len, UP))
-        {
-            pivot = mid_el(*stack_a, len);
-            split_len = split_a(stack_a, stack_b, len, pivot);
-            push_swap(stack_a, stack_b, split_len, 'b');
-            push_swap(stack_a, stack_b, len - split_len, 'a');
-            undo_split_b(stack_a, stack_b, split_len);
-        }
-    }
-    else if (stack_id == 'b' && len == 2)
-    {
-        if ((*stack_b)->value < ((t_element *)(*stack_b)->next)->value)
-        {
-            swap_b(stack_a, stack_b);
-            ft_putstr_fd("sb\n", 1);
-        }
-    }
-    else if (stack_id == 'b' && len > 2)
-    {
-        if (!is_sorted(*stack_b, len, DOWN))
-        {
-            pivot = mid_el(*stack_b, len);
-            split_len = split_b(stack_a, stack_b, len, pivot);
-            push_swap(stack_a, stack_b, split_len, 'a');
-            push_swap(stack_a, stack_b, len - split_len, 'b');
-            undo_split_a(stack_a, stack_b, split_len);
-        }
-    }
 }
 
 int main(int argc, char **argv)
@@ -209,9 +53,5 @@ int main(int argc, char **argv)
     stack_a = crt_stack(&argv[1]);
     stack_b = NULL;
     push_swap(&stack_a, &stack_b, lst_len(stack_a) + 1, 'a');
-    //printf("%d\n", is_sorted(stack_a, lst_len(stack_a), UP));
-//    printf("\nRESULT\n");
-//    lst_print("[A] ", stack_a);
-//    lst_print("[B] ", stack_b);
     return (0);
 }
